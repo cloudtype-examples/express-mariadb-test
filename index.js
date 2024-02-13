95c082a0b1035abad17f5456c75e72e467bdee12
KYEONGMIN CHO <ggingmin@gmail.com>
2024-02-13 15:35:12 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mysql = require("mysql2/promise");
+
+const connection = await mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: 3306,
+  database: "test",
+});
+
+console.log(connection);
+
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.listen(3000);
