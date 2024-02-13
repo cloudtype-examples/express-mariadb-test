4dff9cc8b0d12a57fa575e57982e962f235bdf6d
KYEONGMIN CHO <ggingmin@gmail.com>
2024-02-13 15:52:14 +0900

    commit

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
 const express = require("express");
-const mysql = require("mysql2/promise");
+import mysql, {
+  ConnectionOptions,
+  ResultSetHeader,
+  RowDataPacket,
+} from 'mysql2/promise';
+
+interface User extends RowDataPacket {
+  id: number;
+  name: string;
+}
 
 (async () => {
-    const connection = await mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        port: 3306,
-        database: "test",
-      });
-      
-      console.log(connection);
-})();
+  const connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: 3306,
+    database: "test",
+  });
 
+  await connection.query<ResultSetHeader>("DROP TABLE IF EXISTS `users`;");
+
+  await connection.query<ResultSetHeader>("CREATE TABLE `users` (`id` INT(11) AUTO_INCREMENT, `name` VARCHAR(50), PRIMARY KEY (`id`));");
+
+  const [inserted] = await connection.execute<ResultSetHeader>(
+    "INSERT INTO `users`(`name`) VALUES(?), (?), (?), (?);",
+    ["Josh", "John", "Marie", "Gween"]
+    );
+  
+    console.log('Inserted:', inserted.affectedRows);
+
+    /** Getting users */
+    const [users] = await connection.query<User>(
+      'SELECT * FROM `users` ORDER BY `name` ASC;'
+    );
+  
+    users.forEach((user) => {
+      console.log('-----------');
+      console.log('id:  ', user.id);
+      console.log('name:', user.name);
+    });
+})();
 
 const app = express();
 
